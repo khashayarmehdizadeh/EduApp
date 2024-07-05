3a45108fc234884aeee9a6ecbe3a19155b45cdf7
khashayar mehdizadeh <mzkhashayar@gmail.com>
2024-07-05 23:17:44 +0330

    mkdir Database

--- src/Main/model/bl/TeacherBl.java
+++ src/Main/model/bl/TeacherBl.java
@@ -1,5 +1,6 @@
 package Main.model.bl;
 
+import Main.controller.exceptions.NoTeachertFoundException;
 import Main.model.da.TeacherDa;
 import Main.model.entity.Teacher;
 import Main.model.tools.CRUD;
@@ -9,10 +10,11 @@ import java.util.List;
 
 public class TeacherBl implements CRUD<Teacher> {
     @Getter
-    public static TeacherBl studentBl=new TeacherBl();
+    public static TeacherBl studentBl = new TeacherBl();
+
     @Override
     public Teacher save(Teacher teacher) throws Exception {
-        try (TeacherDa teacherDa=new TeacherDa()){
+        try (TeacherDa teacherDa = new TeacherDa()) {
             teacherDa.save(teacher);
             return teacher;
         }
@@ -22,25 +24,57 @@ public class TeacherBl implements CRUD<Teacher> {
 
     @Override
     public Teacher edit(Teacher teacher) throws Exception {
-        try (TeacherDa teacherDa=new TeacherDa()){
-            if(teacherDa.findById(teacher.getId()))
+        try (TeacherDa teacherDa = new TeacherDa()) {
+            if (teacherDa.findById(teacher.getId()) != null) {
+                teacherDa.edit(teacher);
+                return teacher;
+            } else {
+                throw new NoTeachertFoundException();
+            }
 
         }
-        return null;
+
     }
 
     @Override
     public Teacher remove(int id) throws Exception {
-        return null;
+        try (TeacherDa teacherDa = new TeacherDa()) {
+            Teacher teacher = teacherDa.findById(id);
+            if (teacher != null) {
+                teacherDa.remove(id);
+                return teacher;
+            } else {
+                throw new NoTeachertFoundException();
+            }
+
+        }
     }
 
     @Override
     public List<Teacher> findAll() throws Exception {
-        return null;
+        try (TeacherDa teacherDa = new TeacherDa()) {
+            List<Teacher> teacherList = teacherDa.findAll();
+            if (!teacherList.isEmpty()) {
+                return teacherList;
+            } else {
+                throw new NoTeachertFoundException();
+            }
+
+        }
+
     }
 
     @Override
     public Teacher findById(int id) throws Exception {
-        return null;
+        try (TeacherDa teacherDa = new TeacherDa()) {
+            Teacher teacher = teacherDa.findById(id);
+            if (teacher != null) {
+                return teacher;
+            } else {
+                throw new NoTeachertFoundException();
+            }
+
+        }
+
     }
 }
